fbbf63b1841fdb1a4be02bdf1e4a00127a3dfa6c
Qhaleed <reslykadiri@gmail.com>
2025-04-16 22:52:52 +0800

    Add react-router for the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Router and Routes
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login-page/Login';
+import Home from './pages/home-page/Home';
+import Register from './pages/register-page/Register';
+import AdminHome from './pages/admin-home/AdminHome';
+import AdminDashboard from './pages/admin-dashboard/AdminDashboard';
+import StudentHome from './pages/student-home/StudentHome';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} /> {/* Corrected path */}
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin-home" element={<AdminHome />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/student-home" element={<StudentHome />} />
         </Routes>
       </Router>
     </div>
