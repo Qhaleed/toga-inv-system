50ad83ab3d30cc2320e2dd7edec2945b0a8629a2
kenneth <kennethque101@gmail.com>
2025-04-18 19:27:05 +0800

    palettes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#03003A",
+        secondary: "#252779",
+        tertiary: "#23105A",
+        fourtiary: "#1C113A",
+      },
+      backgroundImage: {
+        'linear-primary': 'linear-gradient(to right, #03003A, #252779, #23105A)',
+        'linear-custom': 'linear-gradient(to bottom right, #1C113A, #23105A, #252779)',
+        'login-bg': 'url("/loginbg.jpg")',
+        'text-gradient-ul': 'linear-gradient(to top left, #C15DEF, #FFFFFF)',
+      },
+    },
   },
   plugins: [],
 }
