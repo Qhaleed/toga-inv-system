470374e4f4d24223393282523ab0ac042da13cde
jonorill <nobnoj20@gmail.com>
2025-04-19 16:39:26 +0800

    clydes styling on regis card
    almost done, just needs line fix, some features for upload image and refinements

--- frontend/src/components/register-card/RegisterCard.js
+++ frontend/src/components/register-card/RegisterCard.js
@@ -32,13 +32,14 @@ export default function RegisterForm() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-login-bg bg-cover bg-no-repeat bg-center p-4 sm:p-6">
-      <div className="w-full max-w-5xl bg-white bg-opacity-30 backdrop-blur-lg rounded-2xl p-6 sm:p-10 shadow-2xl ring-1 ring-white/10">
+      <div className="fixed inset-0 bg-black/60 rounded-l w-full min-h-screen"></div>
+      <div className="w-full max-w-5xl bg-white bg-opacity-20 backdrop-blur-lg rounded-2xl p-6 sm:p-10 shadow-2xl ring-1 ring-white/10">
         <div className="grid grid-cols-1 md:grid-cols-[0.8fr_2.2fr] gap-6">
-          <div className="space-y-4 text-white border-r-2 border-white border-opacity-80 pr-4 sm:pr-6">
-            <h2 className="text-2xl sm:text-3xl font-bold leading-snug">
+          <div className="space-y-4 text-white border-r-2 border-gray-400 pr-4 sm:pr-6">
+            <h2 className="text-2xl sm:text-3xl text-transparent font-figtree font-bold bg-clip-text bg-text-gradient-ul leading-snug">
               Register and <br /> validate your account
             </h2>
-            <ul className="text-sm text-gray-300 list-disc list-inside mt-4 space-y-1">
+            <ul className="text-white text-sm font-regular font-manjari hidden md:block md:text-sm list-disc list-inside mt-4 space-y-1">
               <li>Streamlined management</li>
               <li>Other point</li>
               <li>More flavor text</li>
@@ -48,7 +49,7 @@ export default function RegisterForm() {
           <form onSubmit={handleSubmit} className="space-y-6 text-white">
             {/* STEP 1 */}
             <div>
-              <span className="text-purple-600 text-lg sm:text-xl font-semibold mr-1">
+              <span className="text-purple-300 text-lg sm:text-xl font-figtree font-bold mr-1">
                 STEP 1:
               </span>
               <span className="text-white-50 text-lg sm:text-xl font-semibold">
@@ -58,14 +59,18 @@ export default function RegisterForm() {
             <div className="space-y-4">
               {/* Username */}
               <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4">
-                <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                   Username
                 </label>
                 <input
                   type="text"
                   name="username"
-                  placeholder="Username"
-                  className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                  placeholder="NameOfUser"
+                  className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                   value={formData.username}
                   onChange={handleChange}
                 />
@@ -73,14 +78,18 @@ export default function RegisterForm() {
 
               {/* Password */}
               <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4 relative">
-                <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                   Password
                 </label>
                 <input
                   type={showPassword ? "text" : "password"}
                   name="password"
-                  placeholder="Password"
-                  className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600 pr-10"
+                  placeholder="•••••••••••"
+                  className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                   value={formData.password}
                   onChange={handleChange}
                 />
@@ -94,14 +103,18 @@ export default function RegisterForm() {
 
               {/* Confirm Password */}
               <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4">
-                <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                   Confirm Password
                 </label>
                 <input
                   type="password"
                   name="confirmPassword"
-                  placeholder="Confirm Password"
-                  className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                  placeholder="•••••••••••"
+                  className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                   value={formData.confirmPassword}
                   onChange={handleChange}
                 />
@@ -109,14 +122,18 @@ export default function RegisterForm() {
 
               {/* Email */}
               <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4">
-                <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                   Email
                 </label>
                 <input
                   type="email"
                   name="email"
-                  placeholder="Email"
-                  className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                  placeholder="example@email.com"
+                  className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                   value={formData.email}
                   onChange={handleChange}
                 />
@@ -125,7 +142,7 @@ export default function RegisterForm() {
 
             {/* STEP 2 */}
             <div>
-              <span className="text-purple-600 text-lg sm:text-xl font-semibold mr-1">
+              <span className="text-purple-300 text-lg sm:text-xl font-figtree font-bold mr-1">
                 STEP 2:
               </span>
               <span className="text-white-400 text-lg sm:text-xl font-semibold">
@@ -133,28 +150,36 @@ export default function RegisterForm() {
               </span>
               <div className="grid sm:grid-cols-[2fr_1fr] gap-4 mt-4">
                 <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4">
-                  <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                  <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                     First Name
                   </label>
                   <input
                     type="text"
                     name="firstName"
-                    placeholder="First Name"
-                    className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                    placeholder="Juan Antonio"
+                    className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                     value={formData.firstName}
                     onChange={handleChange}
                   />
                 </div>
 
-                <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4">
-                  <label className="w-full sm:w-16 text-white-400 text-sm font-semibold opacity-80">
+                <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-2">
+                  <label className="w-full sm:w-16 text-white-400 text-m font-semibold opacity-80 font-manjari">
                     M.I.
                   </label>
                   <input
                     type="text"
                     name="middleInitial"
-                    placeholder="M.I."
-                    className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                    placeholder="A."
+                    className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                     value={formData.middleInitial}
                     onChange={handleChange}
                   />
@@ -162,28 +187,36 @@ export default function RegisterForm() {
               </div>
 
               <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4 mt-4">
-                <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                   Surname
                 </label>
                 <input
                   type="text"
                   name="surname"
-                  placeholder="Surname"
-                  className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                  placeholder="Dela Cruz"
+                  className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                   value={formData.surname}
                   onChange={handleChange}
                 />
               </div>
 
               <div className="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-4 mt-4">
-                <label className="w-full sm:w-32 text-white-400 text-sm font-semibold opacity-80">
+                <label className="w-full sm:w-32 text-white-400 text-m font-manjari font-semibold opacity-80">
                   ID Number
                 </label>
                 <input
                   type="text"
                   name="idNumber"
-                  placeholder="ID Number"
-                  className="w-full flex-1 bg-transparent border-b-2 border-white text-white placeholder-gray-300 focus:outline-none focus:border-purple-600"
+                  placeholder="123456"
+                  className="w-full flex-1 bg-white bg-opacity-0 border-b-2 border-gray-300 border-opacity-30 rounded-sm font-manjari
+                          text-white placeholder-gray-300 placeholder:font-manjari
+                          focus:outline-none 
+                          focus:border-purple-600
+                          transition duration-200"
                   value={formData.idNumber}
                   onChange={handleChange}
                 />
@@ -192,7 +225,7 @@ export default function RegisterForm() {
 
             {/* STEP 3 */}
             <div>
-              <span className="text-purple-600 text-lg sm:text-xl font-semibold mr-1 ">
+              <span className="text-purple-300 text-lg sm:text-xl font-figtree font-bold mr-1">
                 STEP 3:
               </span>
               <span className="text-white-400 text-lg sm:text-xl font-semibold">
@@ -214,12 +247,12 @@ export default function RegisterForm() {
                       alt="Upload"
                       className="mx-auto transition-transform ease-in-out duration-300 group-hover:scale-110"
                     />
-                    <p className="text-sm mt-2">
+                    <p className="text-sm mt-2 font-manjari text-white-400 opacity-80">
                       Drag your image here or upload from computer
                     </p>
                     <button
                       type="button"
-                      className="mt-2 px-4 py-1 bg-white text-black rounded-full text-sm font-medium"
+                      className="font-manjari font-bold mt-2 px-4 py-1 bg-white text-black rounded-full text-sm transition hover:scale-105 hover:text-white hover:bg-purple-500"
                     >
                       Browse Computer
                     </button>
@@ -235,7 +268,7 @@ export default function RegisterForm() {
 
             <button
               type="submit"
-              className="w-full bg-purple-600 hover:bg-purple-700 py-3 rounded-full font-semibold text-white transition"
+              className="w-full bg-[#1E1E49] hover:bg-[#45458d] py-2  transition rounded-full font-semibold text-white"
             >
               Register and Verify Account
             </button>
