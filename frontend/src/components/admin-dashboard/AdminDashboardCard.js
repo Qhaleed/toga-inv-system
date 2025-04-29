4f1f17d120f0ca98ce4c149263f4f908c2f5bad9
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-04-29 19:29:32 +0800

    minor updates for side bar and navbar(not complete)

--- frontend/src/components/admin-dashboard/AdminDashboardCard.js
+++ frontend/src/components/admin-dashboard/AdminDashboardCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import SideBar from "../sidebar/SideBar";
-import NavBar from "../navbar/NavBar";
-import Table from "../dashboard/table";
+import Table from "../common/Table";
+import SideBar from "../common/SideBar";
+import NavBar from "../common/NavBar";
 
 
 const AdminDashboardCard = () => {
@@ -9,7 +9,7 @@ const AdminDashboardCard = () => {
         <div className="h-screen overflow-hidden w-screen fixed bg-[#EBEBEB] font-figtree font-medium"> {/*Root Container*/}
             <div className="h-screen fixed w-screen grid grid-cols-4 "> {/*Container of the Left and Right Containers*/}
                 <SideBar /> {/* Left Container and SideBar */}
-                <div className="col-span-3 h-full border border-red-500"> {/* Right Container */}
+                <div className="col-span-3 h-full"> {/* Right Container */}
                     <NavBar />
                     <Table /> {/*Dashboard Table*/}
                 </div>
