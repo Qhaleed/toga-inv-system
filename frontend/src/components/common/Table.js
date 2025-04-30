e0f44e3a15beb5c6c8a3ebc39406646d233a318c
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-05-01 02:43:17 +0800

    minor updates on dahsboard

--- frontend/src/components/common/Table.js
+++ frontend/src/components/common/Table.js
@@ -6,15 +6,15 @@ const Table = ({ isGrid }) => {
     <div className="h-[590px] mt-4 w-full flex justify-center">
       {" "}
       {/*Table Container for Positioning*/}
-      <div className="h-full w-[96%] overflow-auto scroll-style">
+      <div className="h-full w-[96%] overflow-auto scroll-style border-t-2 border-[#02327B]">
         {isGrid ? (
           // Grid mode: 3 columns, 2 rows
           <Rows isGrid={true} />
         ) : (
           // Table mode
-          <table className="table-auto w-full border border-gray-400 ">
+          <table className="table-auto w-full border-x border-gray-400">
             {/*Dashboard Table*/}
-            <thead className="sticky top-0 z-10 bg-[#02327B]">
+            <thead className="sticky top-0 z-10 bg-[#02327B] border-x border-[#02327B]">
               <tr className="text-sm text-white h-10">
                 {/*Table Headers*/}
                 <th>
