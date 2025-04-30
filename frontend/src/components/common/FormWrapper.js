5d0e3cb9aec2e2134112ce8ff5e1b7b964e243dd
jonorill <nobnoj20@gmail.com>
2025-05-01 01:25:08 +0800

    fixed image upload, enhanced styling
    made scroll isolated into content side, will style scrollbar later

--- frontend/src/components/common/FormWrapper.js
+++ frontend/src/components/common/FormWrapper.js
@@ -1,12 +1,16 @@
-import React from "react";
-
-export default function FormWrapper({ title, children, onSubmit }) {
+export default function FormWrapper({ title, children, onSubmit, className }) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-login-bg bg-cover bg-no-repeat bg-center p-4 sm:p-6">
+    <div
+      className={`min-h-screen flex items-center justify-center bg-login-bg bg-cover bg-no-repeat bg-center p-4 sm:p-6 ${className}`}
+    >
       <div className="fixed inset-0 bg-black/50 rounded-l w-full min-h-screen"></div>
-      <div className="w-full max-w-5xl bg-white bg-opacity-50 backdrop-blur-lg rounded-2xl p-6 sm:p-10 shadow-2xl ring-1 ring-white/10">
-        <div className="grid grid-cols-1 md:grid-cols-[0.8fr_2.2fr] gap-6">
-          {/* Titles and All (Left) */}
+      <div className="relative z-10 w-full max-w-5xl bg-white bg-opacity-50 backdrop-blur-lg rounded-2xl p-6 sm:p-10 shadow-2xl ring-1 ring-white/10">
+        <div
+          className={`grid grid-cols-1 md:grid-cols-[0.8fr_2.2fr] gap-6 ${
+            className === "register-card" ? "h-[80vh]" : ""
+          }`}
+        >
+          {/* Left side (fixed) */}
           <div className="space-y-4 text-white border-b-2 md:border-b-0 md:border-r-2 border-white border-opacity-50 pb-4 md:pb-0 pr-0 md:pr-4 sm:pr-6">
             <h2 className="text-2xl sm:text-3xl text-primary font-figtree font-bold leading-snug">
               {title}
@@ -18,8 +22,13 @@ export default function FormWrapper({ title, children, onSubmit }) {
             </ul>
           </div>
 
-          {/* Form Content (Right) */}
-          <form onSubmit={onSubmit} className="space-y-6 text-white">
+          {/* Right side (scrollable form) */}
+          <form
+            onSubmit={onSubmit}
+            className={`space-y-6 text-white ${
+              className === "register-card" ? "overflow-y-auto pr-2" : ""
+            }`}
+          >
             {children}
           </form>
         </div>
