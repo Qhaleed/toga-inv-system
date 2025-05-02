71e37054dca2bf8cbf74dbe7a7090a8142080d2a
kenneth <kennethque101@gmail.com>
2025-05-02 15:37:38 +0800

    updates

--- frontend/src/components/admin-dashboard/AdminDashboardCard.js
+++ frontend/src/components/admin-dashboard/AdminDashboardCard.js
@@ -9,16 +9,27 @@ const AdminDashboardCard = () => {
   const [modifyTable, setmodifyTable] = useState(false);
 
   return (
-    <div className="h-screen overflow-hidden w-screen fixed bg-[#EBEBEB] font-figtree font-medium">
+      <div className="sm:h-screen  hidden sm:overflow-hidden sm:block sm:w-screen sm:fixed sm:bg-[#EBEBEB] font-figtree  sm:font-medium">
+
+      
       {/* ETo pinaka root Container niggas*/}
-      <div className="h-screen fixed w-screen grid grid-cols-4"> {/*<div className="h-screen fixed w-screen grid-cols-4 border border-red-500">*/}
-        <SideBar />
-        <div className="col-span-3 h-full">
+
+      <div className="h-screen fixed sm:w-screen grid grid-cols-4"> {/*<div className="h-screen fixed w-screen grid-cols-4 border border-red-500">*/}
+        <SideBar /> 
+
+
+        <div className="col-span-3   h-full">
+
+
           {/* Right Container */}
-          <div className="w-full flex flex-col items-center" style={{ maxWidth: "98%" }}>
+          <div className="w-full flex flex-col items-center" style={{ maxWidth: "100%" }}>
+
+
             <div className="w-full" style={{ height: "60px", marginBottom: "8px" }}>
               <NavBar isGrid={isGrid} setIsGrid={setIsGrid} modifyTable={modifyTable} setmodifyTable={setmodifyTable}/>
             </div>
+
+            
             <div className="flex justify-center items-start w-full" style={{ height: "600px" }}>
               <Table isGrid={isGrid} modifyTable={modifyTable}/> {/*Dashboard Table*/}
             </div>
