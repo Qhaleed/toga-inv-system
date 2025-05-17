b4b66930c1465db1fcab1bff493b2ceaa2ea8542
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-05-17 15:29:33 +0800

    Checked Return visuals done!

--- frontend/src/components/ui/MissingTasselTooltip.jsx
+++ frontend/src/components/ui/MissingTasselTooltip.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function StocksCapTooltip({ active, payload }) {
+export default function MissingTasselTooltip({ active, payload }) {
   if (!active || !payload || !payload.length) return null;
   const d = payload[0].payload;
   return (
     <div className="rounded-lg bg-white p-3 shadow text-sm border border-gray-200">
       <div className="font-bold mb-1" style={{ color: d.color }}>
-        {d.category}
+        {d.colors}
       </div>
       <div className="font-bold text-lg">{d.count} pcs</div>
     </div>
--- frontend/src/pages/inventory-page/InventoryTabs.jsx
+++ frontend/src/pages/inventory-page/InventoryTabs.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Profile from "../../assets/images/dump.jpg";
-import MyChart from "../../components/ui/my-chart";
+import StocksAllChart from "../../components/ui/StocksAllChart";
 import StocksGownChart from "../../components/ui/StocksGownChart";
 import StocksTasselChart from "../../components/ui/StocksTasselChart";
 import { CarouselPlugin } from "../../components/ui/my-carousel";
 import StocksCapChart from "@/components/ui/StocksCapChart";
 import StocksHoodChart from "@/components/ui/StocksHoodChart";
+import ReturnedAllChart from "@/components/ui/ReturnedAllChart";
+import ReturnedCapChart from "@/components/ui/ReturnedCapChart";
+import ReturnedGownChart from "@/components/ui/ReturnedGownChart";
+import ReturnedHoodChart from "@/components/ui/ReturnedHoodChart";
+import ReturnedTasselChart from "@/components/ui/ReturnedTasselChart";
+import MissingAllChart from "@/components/ui/MissingAllChart";
+import MissingCapChart from "@/components/ui/MissingCapChart";
+import MissingGownChart from "@/components/ui/MissingGownChart";
+import MissingHoodChart from "@/components/ui/MissingHoodChart";
+import MissingTasselChart from "@/components/ui/MissingTasselChart";
 
 export function StocksTab() {
   const [totals, setTotals] = useState({
@@ -150,7 +160,7 @@ export function StocksTab() {
         <div className="w-full flex flex-col md:flex-row items-center gap-8 justify-center mt-5">
           <div className="flex-1 flex flex-col items-center">
             <div className="border border-gray-500 rounded-2xl shadow-lg flex items-center justify-center h-[520px] w-full mb-4">
-              {all && <MyChart />}
+              {all && <StocksAllChart />}
               {cap && <StocksCapChart />}
               {tassel && <StocksTasselChart />}
               {gown && <StocksGownChart />}
@@ -343,24 +353,68 @@ export function ItemStatusTab() {
 
   const totalItems = totals.cap + totals.tassel + totals.gown + totals.hood;
 
-  const [returnToggle, setReturnToggle] = useState(true);
+  const [all, setAll] = useState(true);
+  const [cap, setCap] = useState(false);
+  const [tassel, setTassel] = useState(false);
+  const [gown, setGown] = useState(false);
+  const [hood, setHood] = useState(false);
 
-  const returnToggler = () => {
-    setReturnToggle(true);
+  const allToggle = () => {
+    setAll(true);
+    setCap(false);
+    setTassel(false);
+    setGown(false);
+    setHood(false);
   };
 
-  const notreturnToggler = () => {
-    setReturnToggle(false);
+  const capToggle = () => {
+    setAll(false);
+    setCap(true);
+    setTassel(false);
+    setGown(false);
+    setHood(false);
+  };
+  const tasselToggle = () => {
+    setAll(false);
+    setCap(false);
+    setTassel(true);
+    setGown(false);
+    setHood(false);
+  };
+  const gownToggle = () => {
+    setAll(false);
+    setCap(false);
+    setTassel(false);
+    setGown(true);
+    setHood(false);
+  };
+  const hoodToggle = () => {
+    setAll(false);
+    setCap(false);
+    setTassel(false);
+    setGown(false);
+    setHood(true);
   };
 
-  let returnedBtn = returnToggle
-    ? "bg-[#02327B] text-white h-full w-32 rounded-3xl font-figtree-medium"
-    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-3xl font-figtree-medium transition-all duration-500";
+  let allBtn = all
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
 
-  let notreturnedBtn = !returnToggle
-    ? "bg-[#02327B] text-white h-full w-32 rounded-3xl font-figtree-medium"
-    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-3xl font-figtree-medium transition-all duration-500";
+  let capBtn = cap
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
 
+  let tasselBtn = tassel
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  let gownBtn = gown
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  let hoodBtn = hood
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
   return (
     <>
       {" "}
@@ -378,25 +432,33 @@ export function ItemStatusTab() {
         <div className="w-full flex flex-col md:flex-row items-center gap-8 justify-center mt-5">
           <div className="flex-1 flex flex-col items-start">
             <div className="border border-gray-500 rounded-2xl shadow-lg flex items-center justify-center min-h-[420px] min-w-[600px] max-w-700px] w-full mb-4">
-              <MyChart />
+              <ReturnedAllChart />
             </div>
             <div className="w-full h-10 flex justify-between items-center">
-              <div className="w-[420px] h-10 flex justify-between items-center ml-3">
-                <button className={returnedBtn} onClick={returnToggler}>
-                  Condition
+              <div className="w-[700px] h-10 flex justify-between items-center ml-3">
+                <button className={allBtn} onClick={allToggle}>
+                  All
                 </button>
-                <button className={notreturnedBtn} onClick={notreturnToggler}>
-                  Damage
+                <button className={capBtn} onClick={capToggle}>
+                  Cap
                 </button>
-                <button className={notreturnedBtn} onClick={notreturnToggler}>
-                  Repair
+                <button className={tasselBtn} onClick={tasselToggle}>
+                  Tassel
+                </button>
+                <button className={gownBtn} onClick={gownToggle}>
+                  Gown
+                </button>
+                <button className={hoodBtn} onClick={hoodToggle}>
+                  Hood
                 </button>
               </div>
               <div className="text-[#02327B] text-xl h-8 flex justify-end items-center border-l-2 border-[#F3B51A] mr-3">
                 <h3 className="pl-3">
-                  {returnToggle
-                    ? "Returned Statistics"
-                    : "Not Returned Statistics"}
+                  {all && "All Status"}
+                  {cap && "Cap Status"}
+                  {tassel && "Tassel Status"}
+                  {gown && "Gown Status"}
+                  {hood && "Hood Status"}
                 </h3>
               </div>
             </div>
@@ -408,9 +470,7 @@ export function ItemStatusTab() {
                 {totalItems}
               </span>
               <span className="text-sm text-gray-700 mt-1">
-                {returnToggle
-                  ? "Total Returned Item"
-                  : "Total Unreturned Items"}
+                Total Item Status
               </span>
             </div>
 
@@ -420,7 +480,7 @@ export function ItemStatusTab() {
                 {totals.cap}
               </span>
               <span className="text-sm text-[#dadada] mt-1">
-                {returnToggle ? "Returned Cap" : "Unreturned Cap"}
+                Cap Status
               </span>
               <span className="text-xs text-[#dadada] mt-1">
                 {Object.entries(totals.capSizes || {})
@@ -433,7 +493,7 @@ export function ItemStatusTab() {
                 {totals.tassel}
               </span>
               <span className="text-base text-[#001d5a] mt-1">
-                {returnToggle ? "Returned Tassel" : "Unreturned Tassel"}
+                Tassel Status
               </span>
               <span className="text-xs text-[#001d5a] mt-1">
                 {Object.entries(totals.tasselColors || {})
@@ -446,7 +506,7 @@ export function ItemStatusTab() {
                 {totals.gown}
               </span>
               <span className="text-base text-gray-800 mt-1">
-                {returnToggle ? "Returned Gown" : "Unreturned Gown"}
+                Gown Status
               </span>
               <span className="text-xs text-gray-800 mt-1">
                 {Object.entries(totals.gownSizes || {})
@@ -459,7 +519,7 @@ export function ItemStatusTab() {
                 {totals.hood}
               </span>
               <span className="text-base text-black mt-1">
-                {returnToggle ? "Returned Hood" : "Unreturned Hood"}
+                Hood Status
               </span>
               <span className="text-xs text-black mt-1">
                 {Object.entries(totals.hoodColors || {})
@@ -576,12 +636,75 @@ export function CheckReturnTab() {
   };
 
   let returnedBtn = returnToggle
-    ? "bg-[#02327B] text-white h-full w-32 rounded-3xl font-figtree-medium"
-    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-3xl font-figtree-medium transition-all duration-500";
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium mr-3"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90 mr-3";
 
   let notreturnedBtn = !returnToggle
-    ? "bg-[#02327B] text-white h-full w-32 rounded-3xl font-figtree-medium"
-    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-3xl font-figtree-medium transition-all duration-500";
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  const [all, setAll] = useState(true);
+  const [cap, setCap] = useState(false);
+  const [tassel, setTassel] = useState(false);
+  const [gown, setGown] = useState(false);
+  const [hood, setHood] = useState(false);
+
+  const allToggle = () => {
+    setAll(true);
+    setCap(false);
+    setTassel(false);
+    setGown(false);
+    setHood(false);
+  };
+
+  const capToggle = () => {
+    setAll(false);
+    setCap(true);
+    setTassel(false);
+    setGown(false);
+    setHood(false);
+  };
+  const tasselToggle = () => {
+    setAll(false);
+    setCap(false);
+    setTassel(true);
+    setGown(false);
+    setHood(false);
+  };
+  const gownToggle = () => {
+    setAll(false);
+    setCap(false);
+    setTassel(false);
+    setGown(true);
+    setHood(false);
+  };
+  const hoodToggle = () => {
+    setAll(false);
+    setCap(false);
+    setTassel(false);
+    setGown(false);
+    setHood(true);
+  };
+
+  let allBtn = all
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  let capBtn = cap
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  let tasselBtn = tassel
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  let gownBtn = gown
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
+
+  let hoodBtn = hood
+    ? "bg-[#02327B] text-white h-full w-32 rounded-lg font-figtree-medium"
+    : "border border-[#02327B] text-[#02327B] h-full w-32 rounded-lg font-figtree-medium transition-all duration-500 opacity-70 scale-90";
 
   return (
     <>
@@ -599,24 +722,59 @@ export function CheckReturnTab() {
       <div className="w-full relative h-screen p-8 flex flex-col items-center">
         <div className="w-full flex flex-col md:flex-row items-center gap-8 justify-center mt-5">
           <div className="flex-1 flex flex-col items-start">
-            <div className="border border-gray-500 rounded-2xl shadow-lg flex items-center justify-center min-h-[420px] min-w-[600px] max-w-700px] w-full mb-4">
-              <MyChart />
+            <div className="border border-gray-500 rounded-2xl shadow-lg flex items-center justify-center h-[520px] w-full mb-4">
+              {returnToggle && all && <ReturnedAllChart />}
+              {returnToggle && cap && <ReturnedCapChart />}
+              {returnToggle && tassel && <ReturnedTasselChart />}
+              {returnToggle && gown && <ReturnedGownChart />}
+              {returnToggle && hood && <ReturnedHoodChart />}
+              {!returnToggle && all && <MissingAllChart />}
+              {!returnToggle && cap && <MissingCapChart />}
+              {!returnToggle && tassel && <MissingTasselChart />}
+              {!returnToggle && gown && <MissingGownChart />}
+              {!returnToggle && hood && <MissingHoodChart />}
             </div>
             <div className="w-full h-10 flex justify-between items-center">
-              <div className="w-70 h-10 flex justify-between items-center ml-3">
-                <button className={returnedBtn} onClick={returnToggler}>
-                  Returned
+              <div className="w-[700px] h-10 flex justify-between items-center ml-3">
+                <button className={allBtn} onClick={allToggle}>
+                  All
                 </button>
-                <button className={notreturnedBtn} onClick={notreturnToggler}>
-                  Not Returned
+                <button className={capBtn} onClick={capToggle}>
+                  Cap
+                </button>
+                <button className={tasselBtn} onClick={tasselToggle}>
+                  Tassel
+                </button>
+                <button className={gownBtn} onClick={gownToggle}>
+                  Gown
+                </button>
+                <button className={hoodBtn} onClick={hoodToggle}>
+                  Hood
                 </button>
               </div>
               <div className="text-[#02327B] text-xl h-8 flex justify-end items-center border-l-2 border-[#F3B51A] mr-3">
                 <h3 className="pl-3">
-                  {returnToggle
-                    ? "Returned Statistics"
-                    : "Not Returned Statistics"}
+                  {returnToggle && all && "Returned All"}
+                  {returnToggle && cap && "Returned Cap"}
+                  {returnToggle && tassel && "Returned Tassel"}
+                  {returnToggle && gown && "Returned Gown"}
+                  {returnToggle && hood && "Returned Hood"}
+                  {!returnToggle && all && "Missing All"}
+                  {!returnToggle && cap && "Missing Cap"}
+                  {!returnToggle && tassel && "Missing Tassel"}
+                  {!returnToggle && gown && "Missing Gown"}
+                  {!returnToggle && hood && "Missing Hood"}
                 </h3>
+              </div>
+            </div>
+            <div className="w-full h-10 flex justify-between items-center mt-5">
+              <div className="w-[700px] h-10 flex justify-start items-center ml-3">
+                <button className={returnedBtn} onClick={returnToggler}>
+                  Returned
+                </button>
+                <button className={notreturnedBtn} onClick={notreturnToggler}>
+                  Missing
+                </button>
               </div>
             </div>
           </div>
@@ -629,7 +787,7 @@ export function CheckReturnTab() {
               <span className="text-sm text-gray-700 mt-1">
                 {returnToggle
                   ? "Total Returned Item"
-                  : "Total Unreturned Items"}
+                  : "Total Missing Items"}
               </span>
             </div>
 
@@ -639,7 +797,7 @@ export function CheckReturnTab() {
                 {totals.cap}
               </span>
               <span className="text-sm text-[#dadada] mt-1">
-                {returnToggle ? "Returned Cap" : "Unreturned Cap"}
+                {returnToggle ? "Returned Cap" : "Missing Cap"}
               </span>
               <span className="text-xs text-[#dadada] mt-1">
                 {Object.entries(totals.capSizes || {})
@@ -651,8 +809,8 @@ export function CheckReturnTab() {
               <span className="text-3xl font-bold text-[#001d5a]">
                 {totals.tassel}
               </span>
-              <span className="text-base text-[#001d5a] mt-1">
-                {returnToggle ? "Returned Tassel" : "Unreturned Tassel"}
+              <span className="text-sm text-[#001d5a] mt-1">
+                {returnToggle ? "Returned Tassel" : "Missing Tassel"}
               </span>
               <span className="text-xs text-[#001d5a] mt-1">
                 {Object.entries(totals.tasselColors || {})
@@ -664,8 +822,8 @@ export function CheckReturnTab() {
               <span className="text-3xl font-bold text-gray-800">
                 {totals.gown}
               </span>
-              <span className="text-base text-gray-800 mt-1">
-                {returnToggle ? "Returned Gown" : "Unreturned Gown"}
+              <span className="text-sm text-gray-800 mt-1">
+                {returnToggle ? "Returned Gown" : "Missing Gown"}
               </span>
               <span className="text-xs text-gray-800 mt-1">
                 {Object.entries(totals.gownSizes || {})
@@ -677,8 +835,8 @@ export function CheckReturnTab() {
               <span className="text-3xl font-bold text-black">
                 {totals.hood}
               </span>
-              <span className="text-base text-black mt-1">
-                {returnToggle ? "Returned Hood" : "Unreturned Hood"}
+              <span className="text-sm text-black mt-1">
+                {returnToggle ? "Returned Hood" : "Missing Hood"}
               </span>
               <span className="text-xs text-black mt-1">
                 {Object.entries(totals.hoodColors || {})
